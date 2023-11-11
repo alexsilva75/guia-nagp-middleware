10485db9024ca0f4fdf0b5e31bdd6760df10e831
DESKTOP-EDN7VAJ\alexsandro <alexsilva75>
2023-11-11 19:20:14 -0300

    Stateless auth

--- routes/api.php
+++ routes/api.php
@@ -21,6 +21,10 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::prefix('v1')->group(function () {
     Route::resource('search', \App\Http\Controllers\SearchMidController::class);
     Route::resource('blog-post', \App\Http\Controllers\BlogPostController::class);
+    Route::get('home', function () {
+        return response()->json(['OK']);
+    });
+    Route::post('login', \App\Http\Controllers\TokenLoginController::class);
 });
 
 
